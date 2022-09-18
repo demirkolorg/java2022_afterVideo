9bd62c0df304480dd7e4529feaff4f4dabf0ab27
trliamg <52527207+trliamg@users.noreply.github.com>
2022-09-18 18:22:11 +0300

    strings

--- _9_recapDemo2/src/Main.java
+++ _9_recapDemo2/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] myList = {1.2, 2.5, 3.8, 4.9, 0.7, 8.8, 9.97};
+        double total = 0;
+        double max = myList[0];
+        double min = myList[0];
+
+        for (double number : myList) {
+            if (max < number) max = number;
+
+            if (min > number) min = number;
+
+            total += number;
+        }
+        System.out.println("Total : " + total);
+        System.out.println("Max : " + max);
+        System.out.println("Min : " + min);
+        System.out.println();
     }
 }
