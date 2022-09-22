054ba0b8d62baee18673709cbc293354d2375709
trliamg <52527207+demirkolorg@users.noreply.github.com>
2022-09-22 17:59:04 +0300

    video 30 snu

--- _19_class/src/CustomerManager.java
+++ _19_class/src/CustomerManager.java
@@ -1,2 +1,15 @@
 public class CustomerManager {
+    public void Add() {
+        System.out.println("Müşteri Eklendi");
+    }
+
+    public void Edit() {
+        System.out.println("Müşteri güncellendi");
+
+    }
+
+    public void Delete() {
+        System.out.println("Müşteri silindi");
+
+    }
 }
---
+++ _20_reCapDemoClasses/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(10, 20);
+        System.out.println(sonuc);
+    }
+}
